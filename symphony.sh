8d0ef9e6e9187b0376a95af01f52ddbed9869578
Frédéric Carisey <63855888+Venntus17@users.noreply.github.com>
2020-07-27 11:39:49 +0200

    Update symphony.sh

--- symphony.sh
+++ symphony.sh
@@ -1,7 +1,12 @@
 apt install -y php7.2 libapache2-mod-php7.2 php7.2-common php7.2-sqlite3 php7.2-mysql php7.2-gmp php7.2-curl php7.2-intl php7.2-mbstring php7.2-xmlrpc php7.2-soap php7.2-ldap php7.2-gd php7.2-bcmath php7.2-xml php7.2-cli php7.2-zip
 apt install -y composer
+apt install -i apache2
 
 wget https://get.symfony.com/cli/installer -O - | bash
 
 cd /var/www
 composer create-project symfony/website-skeleton $1
+
+echo "A vous d'allez configuré le DocumentsRoot d'apache !";
+
+cd $1
