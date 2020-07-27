089cd65dd5bed694e8f464db389e19c91a1706a8
Frédéric Carisey <63855888+Venntus17@users.noreply.github.com>
2020-07-27 05:33:25 +0200

    Create set_basic_web_server.sh

---
+++ set_basic_web_server.sh
@@ -0,0 +1,6 @@
+wget -O- http://www.webmin.com/jcameron-key.asc | sudo apt-key add -
+apt_sources_liste="/etc/apt/sources.list"
+echo "deb http://download.webmin.com/download/repository sarge contrib" >> ${apt_sources_liste}
+
+apt update
+apt install apache2 php webmin
